545d9ac3041fbce711da1182d6c002a75efcb964
Tonotix-CZ <rider9812@gmail.com>
2020-07-28 12:52:57 +0300

    added script to marble

---
+++ Marbel run/Assets/Scripts/MarbleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleManager : MonoBehaviour
+{
+    public Rigidbody rb;
+    private float startForce = 10f;
+
+    private void Start()
+    {
+        rb.AddRelativeForce(Vector3.forward * startForce);
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.mass = 5000f;
+            rb.AddRelativeForce(Vector3.forward * 1f);
+            this.gameObject.transform.localScale = new Vector3(4f, 4f, 4f);
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.mass = 1f;
+            rb.AddRelativeForce(Vector3.forward * startForce);
+            this.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        }
+    }
+}
