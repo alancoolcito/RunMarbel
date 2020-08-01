f880d7672f9c446b3a3accd8c3aa467b2b207146
Tonotix-CZ <rider9812@gmail.com>
2020-08-01 15:08:19 +0300

    added camera

--- Marbel run/Assets/Scripts 1/CameraController.cs
+++ Marbel run/Assets/Scripts 1/CameraController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform playerTransform;
+    public GameObject playerTransform;
+    public GameObject Marble;
     [Range(0.01f, 1.0f)]
     public float smoothCamera = 0.5f;
 
@@ -14,15 +15,22 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _cameraOffset = transform.position - playerTransform.position;
+        _cameraOffset = transform.position - Marble.transform.position;
     }
 
     // Update is called once per frame
     private void LateUpdate()
     {
-        Vector3 newPos = playerTransform.position + _cameraOffset;
+        Vector3 newPos = Marble.transform.position + _cameraOffset;
         transform.position = Vector3.Slerp(transform.position, newPos, smoothCamera);
     }
+    private void Update()
+    {
 
+       // transform.rotation = new Quaternion(Marble.transform.rotation.x, playerTransform.transform.rotation.y, playerTransform.transform.rotation.z, playerTransform.transform.rotation.w);
+
+        
+
+    }
 
 }
---
+++ Marbel run/Assets/Scripts 1/CameraController2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController2 : MonoBehaviour
+{
+    public Transform target;
+    public float distance;
+void Update()
+    {
+
+        transform.position = new Vector3(target.position.x, target.position.y, target.position.z - distance);
+        Camera.main.transform.eulerAngles = new Vector3(target.transform.rotation.x, target.transform.rotation.y, 0);
+    }
+}
