6a2018ba7d057c5ac79b1d2f9383352310f1ba73
Tonotix-CZ <rider9812@gmail.com>
2020-08-04 16:15:28 +0300

    changed scale bar and thrust

--- Marbel run/Assets/Scripts 1/MarbleManager.cs
+++ Marbel run/Assets/Scripts 1/MarbleManager.cs
@@ -6,6 +6,33 @@ using UnityEngine.UI;
 public class MarbleManager : MonoBehaviour
 {
     public Rigidbody rb;
+    private float scaleValue = 1f;
+    [SerializeField]
+    private GameObject Marble;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Marble.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+    }
+
+    public void setScale(float scale)
+    {
+        scaleValue = scale;
+        rb.mass = scale;
+    }
+}
+
+
+
+
+
+
+
+/**
     public Slider GameSlider;
 
     private void Start()
@@ -18,7 +45,7 @@ public class MarbleManager : MonoBehaviour
         {
             rb.mass = 1f;
             //rb.AddRelativeForce(Vector3.forward * 10f);
-            this.gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+           // this.gameObject.transform.localScale = new Vector3(3f, 3f, 3f);
             Physics.gravity = new Vector3(0, -1500.0F, 0);
         }
 
@@ -26,7 +53,7 @@ public class MarbleManager : MonoBehaviour
         {
             rb.mass = 250f;
             //rb.AddRelativeForce(Vector3.forward * 8f);
-            this.gameObject.transform.localScale = new Vector3(2f, 2f, 2f);
+            //this.gameObject.transform.localScale = new Vector3(4f, 4f, 4f);
             Physics.gravity = new Vector3(0, -1250.0F, 0);
         }
 
@@ -34,7 +61,7 @@ public class MarbleManager : MonoBehaviour
         {
             rb.mass = 500f;
             //rb.AddRelativeForce(Vector3.forward * 6f);
-            this.gameObject.transform.localScale = new Vector3(3f, 3f, 3f);
+           // this.gameObject.transform.localScale = new Vector3(5f, 5f, 5f);
             Physics.gravity = new Vector3(0, -1000.0F, 0);
         }
 
@@ -42,7 +69,7 @@ public class MarbleManager : MonoBehaviour
         {
             rb.mass = 750f;
             //rb.AddRelativeForce(Vector3.forward * 4f);
-            this.gameObject.transform.localScale = new Vector3(4f, 4f, 4f);
+            //this.gameObject.transform.localScale = new Vector3(6f, 6f, 6f);
             Physics.gravity = new Vector3(0, -750.0F, 0);
         }
 
@@ -50,7 +77,7 @@ public class MarbleManager : MonoBehaviour
         {
             rb.mass = 1000f;
             //rb.AddRelativeForce(Vector3.forward * 3f);
-            this.gameObject.transform.localScale = new Vector3(5f, 5f, 5f);
+            //this.gameObject.transform.localScale = new Vector3(7f, 7f, 7f);
             Physics.gravity = new Vector3(0, -500.0F, 0);
         }
 
@@ -58,7 +85,7 @@ public class MarbleManager : MonoBehaviour
         {
             rb.mass = 1500f;
             //rb.AddRelativeForce(Vector3.forward * 2f);
-            this.gameObject.transform.localScale = new Vector3(5.5f, 5.5f, 5.5f);
+            //this.gameObject.transform.localScale = new Vector3(8f, 8f, 8f);
             Physics.gravity = new Vector3(0, -250.0F, 0);
         }
 
@@ -66,8 +93,8 @@ public class MarbleManager : MonoBehaviour
         {
             rb.mass = 2000f;
             //rb.AddRelativeForce(Vector3.forward * 1f);
-            this.gameObject.transform.localScale = new Vector3(6f, 6f, 6f);
-            Physics.gravity = new Vector3(0, -0.25F, 0);
+            //this.gameObject.transform.localScale = new Vector3(9f, 9f, 9f);
+            Physics.gravity = new Vector3(0, -250F, 0);
         }
     }
 
@@ -86,3 +113,4 @@ public class MarbleManager : MonoBehaviour
     //    rb.AddRelativeForce(Vector3.forward * newSpeed);
     //}
 }
+    */
---
+++ Marbel run/Assets/Scripts 1/SliderScale.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderScale : MonoBehaviour
+{
+    public float thrust = 10f;
+    public Rigidbody rb;
+    private float scaleValue = 1f;
+    [SerializeField]
+    private GameObject Sphere;
+    public GameObject Marble;
+    public Camera followCamera;
+
+    void Start()
+    {
+        Sphere.transform.localScale = new Vector3(4f, 4f, 4f);
+    }
+
+    void Update()
+    {
+        Sphere.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+    }
+
+    public void setScale(float scale)
+    {
+        thrust /= scale;
+        scaleValue = scale;
+        rb.mass = scale*10;
+        rb.AddForce(followCamera.transform.forward * thrust*10);
+
+    }
+}
