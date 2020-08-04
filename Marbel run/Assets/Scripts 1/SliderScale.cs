69ef6cfcc5c6f74cb8e3bec48b06a1bc6f1c9c9a
alancoolcito <alaneroperel@gmail.com>
2020-08-04 17:30:40 +0300

    Work in progress

--- Marbel run/Assets/Scripts 1/SliderScale.cs
+++ Marbel run/Assets/Scripts 1/SliderScale.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class SliderScale : MonoBehaviour
 {
-    public float thrust = 10f;
+    public float thrust = 5f;
     public Rigidbody rb;
-    private float scaleValue = 1f;
+    private float scaleValue = 4f;
+    private Vector3 scaleVector;
+
+    public float newThrust;
+
     [SerializeField]
     private GameObject Sphere;
     public GameObject Marble;
@@ -19,15 +23,22 @@ public class SliderScale : MonoBehaviour
 
     void Update()
     {
+        scaleVector = new Vector3(scaleValue, scaleValue, scaleValue);
+
         Sphere.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+
+        newThrust = thrust / scaleValue;
+
+        //rb.AddForce(followCamera.transform.forward * newThrust * 300);
+
+        rb.AddForce(followCamera.transform.forward * newThrust/2, ForceMode.VelocityChange);
+
     }
 
     public void setScale(float scale)
     {
-        thrust /= scale;
+        //thrust /= scale;
         scaleValue = scale;
-        rb.mass = scale*10;
-        rb.AddForce(followCamera.transform.forward * thrust*10);
-
+        rb.mass = scale*10; 
     }
 }
