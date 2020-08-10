45aa717218bbba5467129636d6e9342afd3887f1
YotamBenda <59575705+YotamBenda@users.noreply.github.com>
2020-08-10 16:12:34 +0300

    RankingManager progress

---
+++ Marbel run/Assets/Scripts 1/RankingManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] checkPoints;
+    [SerializeField]
+    private GameObject[] allMarbels;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
