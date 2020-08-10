4ad5e6e474b596341a47d77af19596ccb8296117
Tonotix-CZ <rider9812@gmail.com>
2020-08-10 18:12:11 +0300

    added speed Booster line

--- Marbel run/Assets/Scripts 1/SliderScale.cs
+++ Marbel run/Assets/Scripts 1/SliderScale.cs
@@ -7,7 +7,7 @@ public class SliderScale : MonoBehaviour
     public float thrust = 5f;
     public Rigidbody rb;
     private float scaleValue = 3f;
-    private Vector3 scaleVector;
+   // private Vector3 scaleVector;
 
     public float newThrust;
 
@@ -20,12 +20,13 @@ public class SliderScale : MonoBehaviour
 
     void Start()
     {
+        Physics.gravity = new Vector3(0, -10F, 0);
         Sphere.transform.localScale = new Vector3(3f, 3f, 3f);
     }
 
     void Update()
     {
-        scaleVector = new Vector3(scaleValue, scaleValue, scaleValue);
+       // scaleVector = new Vector3(scaleValue, scaleValue, scaleValue);
 
         Sphere.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
 
@@ -34,24 +35,24 @@ public class SliderScale : MonoBehaviour
 
         rb.AddForce(followCamera.transform.forward * newThrust/4, ForceMode.VelocityChange);
 
-        if (joystick.Vertical >= .3f && scaleValue <= 6f)
+        if (joystick.Vertical >= .1f && scaleValue <= 6f)
         {
-            scaleValue += 0.05f;
+            scaleValue += 0.08f;
         }
 
-        if (joystick.Vertical <= -.3f && scaleValue >= 1f)
+        if (joystick.Vertical <= -.1f && scaleValue >= 1f)
         {
-            scaleValue -= 0.05f;
+            scaleValue -= 0.08f;
         }
 
         if(joystick.Horizontal >= .1)
         {
-            rb.AddForce(followCamera.transform.right * 1.5f, ForceMode.Acceleration);
+            rb.AddForce(followCamera.transform.right * 3f, ForceMode.Acceleration);
         }
 
         if (joystick.Horizontal <= -.1)
         {
-            rb.AddForce(followCamera.transform.right * -1.5f, ForceMode.Acceleration);
+            rb.AddForce(followCamera.transform.right * -3f, ForceMode.Acceleration);
         }
     }
 
---
+++ Marbel run/Assets/Scripts 1/SpeedBoosterPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpeedBoosterPlatform : MonoBehaviour
+{
+    public float boost = 1000f;
+    public Rigidbody rb;
+    public Rigidbody rbAI;
+
+
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if ((other.gameObject.tag == "Player") || (other.gameObject.tag == "AI"))
+            {
+            rb.AddForce(boost,0,0,ForceMode.Impulse);
+            rbAI.AddForce(boost, 0, 0, ForceMode.Impulse);
+           // other.attachedRigidbody.AddForce(boost, 0, 0, ForceMode.Impulse);
+        }
+    }    
+}
