2f6b30c98f2ade4e3017c905dcbbe847cdc47a33
Tonotix-CZ <rider9812@gmail.com>
2020-08-15 20:44:39 +0300

    added main menu UI

---
+++ Marbel run/Assets/Scripts 1/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private bool paused = false;
+
+    public void PlaGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuiGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+    public void PauseGame()
+    {
+        if (paused == false)
+        {
+            Time.timeScale = 0;
+            paused = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            paused = !paused;
+        }
+    }
+}
