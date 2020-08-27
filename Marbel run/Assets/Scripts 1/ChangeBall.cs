8dd2c2db87a7b25910f48b522f0a00726b7684f3
alancoolcito <alaneroperel@gmail.com>
2020-08-27 18:00:32 +0300

    ball color

---
+++ Marbel run/Assets/Scripts 1/ChangeBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBall : MonoBehaviour
+{
+    private GameObject Player;
+    static Material PlayerMat;
+    public Material FlagMat;
+    public Material PrideMat;
+    public Material GermanyMat;
+    public Material ClassicMat;
+
+
+
+    void Start()
+    {
+
+    }
+
+    public void FlagBall()
+    {
+        PlayerMat = FlagMat;
+    }
+    public void PrideBall()
+    {
+        PlayerMat = PrideMat;
+    }
+    public void GermanyBall()
+    {
+        PlayerMat = GermanyMat;
+    }
+    public void ClassicBall()
+    {
+        PlayerMat = ClassicMat;
+    }
+}
